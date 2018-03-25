5fd1a3c07c44c2f2fe9cdc75a7d1d16ede8db292
Jolaade Adewale <jolaade.adewale@andela.com>
2018-03-25 16:30:28 +0100

    test auto deploy

--- src/container/user/scenes/signUp/index.js
+++ src/container/user/scenes/signUp/index.js
@@ -11,7 +11,7 @@ class SignUp extends Component {
     evt.preventDefault();
     const { email, password } = this.props.admin.adminProfile;
 
-    if (email === 'jolaade@yahoo.com' && password === 'tester') {
+    if (email === 'jolaade@yahoo.com' && password === 'testers') {
       this.props.updateSignUp('error', '');
       this.props.signUpSuccess();
       return;
